0531c044dcb7d337760c2fb9e39b378e8fad97d0
Pablo Valença <pablo.valenca@bfcdigital.com.br>
2022-11-05 23:41:32 -0300

    feat: configuração do tema claro e provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export const AppRoutes = () => {
   
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button>TESTE</Button>} />
+      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>TESTE</Button>} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
